3fab84b2d95fcf8182b87401261d42b6d92c436e
rkleczek897 <rkleczek897@gmail.com>
2022-04-08 08:36:48 +0100

    nice

---
+++ footer.php
@@ -0,0 +1,13 @@
+<!-- #content, #contentWrapper --></div></div>
+
+	<div id="sideBar">
+		SideBar Content Here
+	<!-- #sideBar --></div>
+
+	<div id="footer">
+		&copy Whoever, All rights reserved.
+	<!-- #footer --></div>
+
+<!-- #pageWrapper --></div>
+
+</body></html>
---
+++ header.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html
+	xmlns="http://www.w3.org/1999/xhtml"
+	lang="en"
+	xml:lang="en"
+><head>
+
+<meta
+	http-equiv="Content-Type"
+	content="text/html; charset=utf-8"
+/>
+
+<meta
+	http-equiv="Content-Language"
+	content="en"
+/>
+
+<link
+	type="text/css"
+	rel="stylesheet"
+	href="CSS_code.css"
+	media="screen,projection,tv"
+/>
+
+<title>
+<?php
+	echo isset($pageName) ? $pageName.' - ' : ''
+?>
+	Site Name
+</title>
+
+</head><body>
+
+<div id="pageWrapper">
+
+	<h1>
+		Site Name
+		<span><!-- image replacement --></span>
+	</h1>
+
+	<ul id="mainMenu">
+		<li><a href="#">Home</a></li>
+		<li><a href="#">Forums</a></li>
+		<li><a href="#">Links</a></li>
+		<li><a href="#">Contact</a></li>
+	</ul>
+
+	<div id="contentWrapper"><div id=content">
